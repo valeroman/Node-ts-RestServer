d41c2ca0409c537da53e96d1129929a58d6b15cc
Roman Valero <valeroman@gmail.com>
2021-05-31 11:06:35 -0500

    delete index dist

--- middlewares/index.ts
+++
@@ -1,6 +0,0 @@
-
-export * from '../middlewares/validate-jwt';
-export * from '../middlewares/valid-role';
-export * from '../middlewares/valid-field';
-
-// export { default as useClickedOutside } from '../middlewares/validate-jwt';
